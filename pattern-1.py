cd4e03bb8eff034da59c53233e8e02957185c5fe
alpana singh <alpanasinghcs@gmail.com>
2025-06-19 00:34:38 +0530

    pattern: right-angled number triangle

---
+++ pattern-1.py
@@ -0,0 +1,6 @@
+def right_angled_number_triangle(n):
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+right_angled_number_triangle(5)
