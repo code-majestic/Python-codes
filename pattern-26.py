bca9025eda30bdede94d8fdbdc5534b1e8457fc2
alpana singh <alpanasinghcs@gmail.com>
2025-07-23 19:37:05 +0530

    Print plus sign with star

---
+++ pattern-26.py
@@ -0,0 +1,8 @@
+rows = 5
+for i in range(rows):
+    for j in range(rows):
+        if i == rows // 2 or j == rows // 2:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
